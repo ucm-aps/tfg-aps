9760162fbe4bb079246f3fd98c2c5239fe53ccc0
kun-sys <73662477+kun-sys@users.noreply.github.com>
2022-11-26 16:34:00 +0100

    Front Notificacion

--- src/app/pages/shared/navbar/navbar.component.ts
+++ src/app/pages/shared/navbar/navbar.component.ts
@@ -7,7 +7,8 @@ import {UsuarioService} from '../../../services/usuario.service';
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+    styleUrls: ['./navbar.component.scss'
+            ]
 })
 export class NavbarComponent implements OnDestroy {
 
