7b4e8250970743661f5bfc0435af8a3e38113226
emptywhity <99326402+emptywhity@users.noreply.github.com>
2023-03-18 17:59:30 +0100

    Nuevo diseño listado notificaciones

--- src/app/pages/checkbox-notificaciones/checkbox-notificaciones.component.ts
+++ src/app/pages/checkbox-notificaciones/checkbox-notificaciones.component.ts
@@ -11,44 +11,62 @@ import { UsuarioService } from '../../services/usuario.service';
   styleUrls: ['./checkbox-notificaciones.component.scss']
 })
 export class CheckboxNotificacionesComponent implements OnInit {
-  public offset = 0
-  public limit = 50
-  public filterUsuario = {}
-  public totalNotificaciones = 0
-  public notificaciones: Notificacion[]
-  public totalNotificacionesBuscadas = 0
-  public cargando = false
-
-  
+  public offset = 0;
+  public limit = 2; // Cambia este valor para ajustar el número de notificaciones por página.
+  public filterUsuario = {};
+  public totalNotificaciones = 0;
+  public notificaciones: Notificacion[] = [];
+  public totalNotificacionesBuscadas = 0;
+  public cargando = false;
+  public paginacion = 1; // Define la página actual.
 
   constructor(
-    public notificacionService:NotificacionService,
+    public notificacionService: NotificacionService,
     public usuarioService: UsuarioService
-
   ) { }
 
   ngOnInit(): void {
     this.cargarNotificacion();
   }
 
-  getFiltros(){
-    return{
+  getFiltros() {
+    return {
       usuario: this.filterUsuario
     }
   }
+
   cargarNotificacion(): void {
+    this.cargando = true;
     this.notificacionService
-    .cargarNotificaciones(this.offset, this.limit, this.getFiltros(), this.usuarioService.usuario.uid)
+      .cargarNotificaciones(this.offset, this.limit, this.getFiltros(), this.usuarioService.usuario.uid)
       .subscribe(({ total, filtradas, notificaciones }) => {
-        this.totalNotificaciones = total.valueOf()
-        this.totalNotificacionesBuscadas = filtradas.valueOf()
-        this.notificaciones = notificaciones
-        this.cargando = false
-      })
+        this.totalNotificaciones = total.valueOf();
+        this.totalNotificacionesBuscadas = filtradas.valueOf();
+        this.notificaciones = notificaciones;
+        this.cargando = false;
+      });
   }
 
-  DesactivarNotificacion(): void{
+  DesactivarNotificacion(): void {
     this.notificaciones = [];
+  }
+
+  // Cambia a la página siguiente.
+  siguientePagina() {
+    if (this.paginacion * this.limit < this.totalNotificaciones) {
+      this.paginacion++;
+      this.offset = (this.paginacion - 1) * this.limit;
+      this.cargarNotificacion();
+    }
+  }
+
+  // Cambia a la página anterior.
+  paginaAnterior() {
+    if (this.paginacion > 1) {
+      this.paginacion--;
+      this.offset = (this.paginacion - 1) * this.limit;
+      this.cargarNotificacion();
+    }
   }
 
 }
