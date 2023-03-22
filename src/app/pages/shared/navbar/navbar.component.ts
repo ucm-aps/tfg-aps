d63f460c6f59174082d0e63796a82abdf72321fa
emptywhity <99326402+emptywhity@users.noreply.github.com>
2023-03-22 17:55:53 +0100

    Revert "timbre"
    This reverts commit fbf35994c14cb409d47b328ab935c1d99bbf23e6.

--- src/app/pages/shared/navbar/navbar.component.ts
+++ src/app/pages/shared/navbar/navbar.component.ts
@@ -3,8 +3,6 @@ import {Router, ActivationEnd} from '@angular/router';
 import {filter, map} from 'rxjs/operators';
 import {Subscription} from 'rxjs';
 import {UsuarioService} from '../../../services/usuario.service';
-import { Notificacion } from 'src/app/models/notificacion.model';
-import { NotificacionService } from 'src/app/services/notificacion.service';
 
 @Component({
     selector: 'app-navbar',
@@ -15,14 +13,12 @@ import { NotificacionService } from 'src/app/services/notificacion.service';
 export class NavbarComponent implements OnDestroy {
 
     public routeExtraData$: Subscription;
-    public notificaciones: Notificacion[];
 
-    constructor(public usuarioService: UsuarioService, private router: Router, public notificacionService: NotificacionService) {
+    constructor(public usuarioService: UsuarioService, private router: Router) {
         this.routeExtraData$ = this.getRouteExtraData()
             .subscribe(({titulo}) => {
                 document.title = titulo;
             });
-        this.cargarNotificacion();
     }
 
     ngOnDestroy(): void {
@@ -70,13 +66,5 @@ export class NavbarComponent implements OnDestroy {
     mostrarPartenariados() {
         return this.usuarioService.usuario && (this.usuarioService.usuario.esGestor
             || this.usuarioService.usuario.esSocioComunitario);
-    }
-
-    cargarNotificacion(){
-        this.notificacionService.cargarNotificaciones(this.usuarioService.usuario.uid).subscribe(({ total,  notificaciones }) => {
-            this.notificaciones = notificaciones;
-            this.notificaciones.map(n => n.leido == "1");
-        });
-
     }
 }
